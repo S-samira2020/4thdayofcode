8097c3c1ff95796f06ca7289e676fbacd707b1d2
Samira <54632872+S-samira2020@users.noreply.github.com>
2021-08-04 21:25:17 +0600

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+int myself(int arr[], int sz)
+{
+    if(sz == 1)
+    {
+        return arr[0];
+    }
+    else
+    {
+        int myvalue = arr[sz-1];
+        int colvalue = myself(arr, sz-1);
+        if(myvalue < colvalue)
+        {
+            return myvalue;
+        }
+        else
+        {
+            return colvalue;
+        }
+    }
+}
+
+int main()
+{
+     int arr[] = {100, 50, 1000, -50, 150, 2000};
+     int result = myself(arr, 6);
+     cout << "the maximum value is : " << result << endl;
+
+    return 0;
+}
